50c37cd1cbeb53e8cc3e2c731c598bed6268e879
irhw110 <harisw110@gmail.com>
2018-11-11 17:00:09 +0700

    2d

--- fungsi2d.py
+++ fungsi2d.py
@@ -1,9 +1,29 @@
+import math
+
+
+def kalimatriks (vertices,transformasi) :
+    brs = []
+
+    for vertice in vertices :
+        kol = []
+        for j in range (len(vertices)) :
+            sum = 0
+            for k in range (0,3) :
+                sum = sum + (vertice[k]*transformasi[j][k])
+            kol.append(sum)
+        brs.append(kol)
+    return brs
+
 def translate (vertices,a,b) :
-    translasi = []
-    for i in range (len(vertices)) :
-        translasi.append([a,b,0])
+    translasi = [[1,0,a],[0,1,b],[0,0,1]]
 
     for i in range (len(vertices)) :
         vertices[i][0] = vertices[i][0] + translasi[i][0]
         vertices[i][1] = vertices[i][1] + translasi[i][1]
     return vertices
+
+def rotate (vertices,r,a,b) :
+    r = math.radians(r)
+    rotasi = [[math.cos(r),-(math.sin(r)),0],[math.sin(r),math.cos(r),0],[0,0,1]]
+
+    return kalimatriks(vertices,rotasi)
